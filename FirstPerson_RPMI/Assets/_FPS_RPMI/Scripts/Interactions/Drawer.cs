5a7ee84b4e68acdd5ba561994366272b270e6623
IsaacRubio <isaacrubioalfonso@gmail.com>
2026-04-21 00:35:44 +0200

    Drawer/Drag+ animations working

--- FirstPerson_RPMI/Assets/_FPS_RPMI/Scripts/Interactions/Drawer.cs
+++ FirstPerson_RPMI/Assets/_FPS_RPMI/Scripts/Interactions/Drawer.cs
@@ -3,20 +3,18 @@ using UnityEngine;
 public class Drawer : Interactable
 {
     public Animator anim;
-    bool isOpen = false;
 
-    void Start()
-    {
-        interactionText = "Open";
-    }
+    bool isOpen = false;
 
     public override void Interact()
     {
         base.Interact();
 
         isOpen = !isOpen;
-        anim.SetBool("Open", isOpen);
 
-        SetInteractionText(isOpen ? "Close" : "Open");
+        if (isOpen)
+            anim.SetTrigger("Open");
+        else
+            anim.SetTrigger("Close");
     }
 }
